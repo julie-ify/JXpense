5266e22a557d4ae76a1bfe9dee6299c9b0510ec2
Juliana Ifionu <julieifionu@gmail.com>
2022-01-01 19:58:09 +0100

    Configure route for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  # get 'categories/index'
+  # get 'splash/index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  authenticated :user do
+    root 'categories#index', as: :authenticated_root
+  end
+
+  unauthenticated :user do
+    root to: 'splash#index'
+  end
 end
