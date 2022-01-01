19aaec8d487bedf21ee6b568451f03905b5b458a
Juliana Ifionu <julieifionu@gmail.com>
2022-01-01 17:55:32 +0100

    Add validation and relationships to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+
+  validates :name,  presence: true, length: { maximum: 50 }
+  validates :email, presence: true
+
+  has_many :categories
+  has_many :products
 end
