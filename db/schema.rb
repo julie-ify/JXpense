40dc23104a971f53e50c7e0f6f31df4f34caa2d8
Juliana Ifionu <julieifionu@gmail.com>
2022-01-01 17:50:06 +0100

    Generate database migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_01_01_164714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "icon", null: false
+    t.datetime "created_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "payments", force: :cascade do |t|
+    t.bigint "category_id"
+    t.bigint "product_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_payments_on_category_id"
+    t.index ["product_id"], name: "index_payments_on_product_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name", null: false
+    t.decimal "amount", default: "0.0", null: false
+    t.datetime "created_at", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_products_on_author_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "categories", "users"
+  add_foreign_key "products", "users", column: "author_id"
 end
