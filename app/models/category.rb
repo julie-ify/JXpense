acc939629a73a8898f14f8c8f2c09f2e1480174f
Juliana Ifionu <julieifionu@gmail.com>
2022-01-01 19:51:58 +0100

    Create category model, add validations and associations

---
+++ app/models/category.rb
@@ -0,0 +1,10 @@
+class Category < ApplicationRecord
+ 
+  validates :name,  presence: true, length: { maximum: 50 }
+  validates :icon,  presence: true
+
+  belongs_to :user
+
+  has_many :payments
+  has_many :products, through: :payments
+end
