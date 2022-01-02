97443480d06ef9c2bc6b091017fa9edd193296d2
Juliana Ifionu <julieifionu@gmail.com>
2022-01-02 13:46:56 +0100

    Add show and new actions for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,32 @@
 class CategoriesController < ApplicationController
+  load_and_authorize_resource
+  
   def index
+    @categories = current_user.categories
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    @products = @category.products.order(created_at: :desc)
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = current_user.categories.new(category_params)
+
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to categories_url, notice: 'Category was successfully created!.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
   end
 end
