9c10b1a0c1ddc76ebdc137e34c24b0286f1c61ab
Juliana Ifionu <julieifionu@gmail.com>
2023-10-26 14:07:16 -0400

    Fix deploy build error

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,6 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
+RAILS_ENV=production bundle exec rake db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+bundle exec rake db:create
 bundle exec rake db:migrate
