c5d4949b44f350b7d9b60f215b801b386a4e6ecc
Juliana Ifionu <julieifionu@gmail.com>
2024-02-08 14:18:30 -0500

    Fix build error

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,9 +3,6 @@
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-bundle exec rails db:drop
-bundle exec rails db:create
-bundle exec rails db:migrate
-bundle exec rails db:seed
+./bin/rails assets:precompile
+./bin/rails assets:clean
+./bin/rails db:drop db:create db:migrate db:seed
